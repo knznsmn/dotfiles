9b40f31b561101158f8b01ace30b1517f90c2926
Julius C. Cesar <knznsmn@icloud.com>
2024-10-25 00:45:13 +0400

    Add header function

--- scripts/n1.sh
+++ scripts/n1.sh
@@ -1,10 +1,43 @@
 #!/usr/bin/env bash
+function catHeader() {
+  local author="Written by Julius Cinco Cesar"
+  local date=$(date +"%B %d, %Y")
+  local hr_lens=49
+
+  if [ "$1" = "html" ]; then
+    local br_ouve="<!"
+    local br_ferm="->"
+    local hr_bars="-"
+  else
+    local br_ouve="/*"
+    local br_ferm="*/"
+    local hr_bars="*"
+  fi
+
+  echo -n "$br_ouve"
+  local i=0
+  while [ $i -lt $hr_lens ]; do
+    echo -n "$hr_bars"
+    (( i++ ));
+  done
+  echo -e "\n\n"
+  echo -e "\t$author"
+  echo -e "\t$date"
+  echo -e "\n"
+  local i=0
+  while [ $i -lt $hr_lens ]; do
+    echo -n "$hr_bars"
+    (( i++ ));
+  done
+  echo -e "$br_ferm"
+}
 
 function initProjet() {
 
 mkdir assets
 
 cat <<EOL > index.html
+$(catHeader "html")
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -24,6 +57,7 @@ EOL
 mkdir -p assets/css assets/js
 
 cat <<EOL > assets/css/styles.css
+$(catHeader)
 * {
   margin: 0;
   padding: 0;
@@ -31,15 +65,20 @@ cat <<EOL > assets/css/styles.css
 }
 EOL
 
-echo "// Element Bindings" > assets/js/main.js
+cat <<EOL > assets/js/main.js
+$(catHeader)
+
+// Elements Bindings
+
+EOL
 
 echo "Project structure has been created successfully."
 }
 
 function showMenu() {
-  echo "Usage: $0 [PARAM] [PARAM]"
+  echo -e "\nSyntax: $0 [PARAM] [PARAM]\n"
   echo -e "\t -i"
-  echo -e "\t -h"
+  echo -e "\t -h\n"
 }
 
 # MAIN()
